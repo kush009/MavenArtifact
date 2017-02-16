709e396caae425f0b6c15b31191979287c809465
kush009 <kushsingh9@gmail.com>
2017-02-16 17:10:03 +0530

    maven project

---
+++ src/site/@H@/src/Introduction.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class Introduction {
+	/*------@TestInfo ------------
+	 * ---Name : Kaushlendra
+	 */
+	@Test
+	public void nameInfo(){
+		String name="Kaushlendra";
+		System.out.println(name);
+	}
+	
+
+}
